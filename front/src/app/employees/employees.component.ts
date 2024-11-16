837187d7214388f871f37e19117305aabd432def
Jalil <abdullacelil00@gmail.com>
2024-11-16 13:23:17 +0400

    Update & delete employee

--- front/src/app/employees/employees.component.ts
+++ front/src/app/employees/employees.component.ts
@@ -16,9 +16,11 @@ export class EmployeesComponent {
   employees: Employee[] = [];
   loading: boolean = true;
   url: string = 'http://localhost:8000/api/employees';
-  name: string = '';
-  address: string = '';
-  phone: string = '';
+  name: string | null = '';
+  address: string | null = '';
+  phone: string | null = '';
+  id: number | null = null;
+  updateMode: boolean = false;
 
   constructor(private http: HttpClient) {
     this.getEmployees();
@@ -37,12 +39,47 @@ export class EmployeesComponent {
       address: this.address,
       phone: this.phone
     }
-
-    this.http.post(this.url, body).subscribe((result: any) => {
+    this.http.post(this.url, body).subscribe(() => {
       this.getEmployees();
       this.name = '';
       this.address = '';
       this.phone = '';
     });
+  }
+
+  setUpdate(data: Employee) {
+    this.name = data.name;
+    this.address = data.address;
+    this.phone = data.phone
+    this.id = data.id;
+  }
+
+  update() {
+    let body = {
+      name: this.name,
+      address: this.address,
+      phone: this.phone
+    }
+    this.http.put(this.url + '/' + this.id, body).subscribe(() => {
+      this.getEmployees();
+      this.name = '';
+      this.address = '';
+      this.phone = '';
+      this.id = null;
+    })
+  }
+
+  save() {
+    if (this.id) {
+      this.update();
+    } else {
+      this.register();
+    }
+  }
+
+  delete(id: number) {
+    this.http.delete(this.url + '/' + id).subscribe(() => {
+      this.getEmployees();
+    })
   }
 }
